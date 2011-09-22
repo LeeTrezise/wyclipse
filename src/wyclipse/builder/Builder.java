7d2152f93ce3ac6187164914e266fb270ef01f74
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-22 15:21:45 +1200

    Minor tweak.

--- src/wyclipse/builder/Builder.java
+++ src/wyclipse/builder/Builder.java
@@ -1,7 +1,15 @@
 package wyclipse.builder;
 
+import java.util.Map;
+
 import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 
 public class Builder extends IncrementalProjectBuilder {
 
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
+			throws CoreException {
+		return null;
+	}
 }
