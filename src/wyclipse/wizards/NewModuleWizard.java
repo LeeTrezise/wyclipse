375f7d8d57bd7bdf33786e680c51cd159d8b1e4c
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-26 17:50:03 +1300

    Working on the new module wizard.

--- src/wyclipse/wizards/NewModuleWizard.java
+++ src/wyclipse/wizards/NewModuleWizard.java
@@ -1,46 +1,150 @@
 package wyclipse.wizards;
 
-import org.eclipse.jface.wizard.*;
-import org.eclipse.swt.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.operation.*;
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import java.io.*;
+import org.eclipse.ui.*;
+import org.eclipse.ui.ide.IDE;
 
-public class NewModuleWizard extends Wizard {
-	private MainPage mainPage;
+/**
+ * This is a sample new wizard. Its role is to create a new file 
+ * resource in the provided container. If the container resource
+ * (a folder or a project) is selected in the workspace 
+ * when the wizard is opened, it will accept it as the target
+ * container. The wizard creates one file with the extension
+ * "mpe". If a sample multi-page editor (also available
+ * as a template) is registered for the same extension, it will
+ * be able to open it.
+ */
+
+public class NewModuleWizard extends Wizard implements INewWizard {
+	private NewModulePage page;
+	private ISelection selection;
+
+	/**
+	 * Constructor for WhileyModuleNewWizard.
+	 */
+	public NewModuleWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
 	
-	
+	/**
+	 * Adding the page to the wizard.
+	 */
+
 	public void addPages() {
-		mainPage = new MainPage("Whiley Module Page");
-		addPage(mainPage);
+		page = new NewModulePage(selection);
+		addPage(page);
 	}
-	
+
+	/**
+	 * This method is called when 'Finish' button is pressed in
+	 * the wizard. We will create an operation and run it
+	 * using wizard as execution context.
+	 */
 	public boolean performFinish() {
-		return false;
+		final String containerName = page.getContainerName();
+		final String fileName = page.getFileName();
+		IRunnableWithProgress op = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				try {
+					doFinish(containerName, fileName, monitor);
+				} catch (CoreException e) {
+					throw new InvocationTargetException(e);
+				} finally {
+					monitor.done();
+				}
+			}
+		};
+		try {
+			getContainer().run(true, false, op);
+		} catch (InterruptedException e) {
+			return false;
+		} catch (InvocationTargetException e) {
+			Throwable realException = e.getTargetException();
+			MessageDialog.openError(getShell(), "Error", realException.getMessage());
+			return false;
+		}
+		return true;
 	}
 	
-	private static class MainPage extends WizardPage {
-		private Text sourceFolder;
-		private Text pkg;
-		private Text name;
+	/**
+	 * The worker method. It will find the container, create the
+	 * file if missing or just replace its contents, and open
+	 * the editor on the newly created file.
+	 */
 
-		protected MainPage(String pageName) {
-			super(pageName);
-			setTitle("Whiley Module");
-			setDescription("Create a new Whiley module.");
+	private void doFinish(
+		String containerName,
+		String fileName,
+		IProgressMonitor monitor)
+		throws CoreException {
+		// create a sample file
+		monitor.beginTask("Creating " + fileName, 2);
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		if (!resource.exists() || !(resource instanceof IContainer)) {
+			throwCoreException("Container \"" + containerName + "\" does not exist.");
 		}
+		IContainer container = (IContainer) resource;
+		final IFile file = container.getFile(new Path(fileName));
+		try {
+			InputStream stream = openContentStream();
+			if (file.exists()) {
+				file.setContents(stream, true, true, monitor);
+			} else {
+				file.create(stream, true, monitor);
+			}
+			stream.close();
+		} catch (IOException e) {
+		}
+		monitor.worked(1);
+		monitor.setTaskName("Opening file for editing...");
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page =
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+		monitor.worked(1);
+	}
+	
+	/**
+	 * We will initialize file contents with a sample text.
+	 */
 
-		public void createControl(Composite parent) {
-			Composite composite = new Composite(parent, SWT.NONE);
-			GridLayout layout = new GridLayout();
-			layout.numColumns = 2;
-			composite.setLayout(layout);
-			setControl(composite);
-			new Label(composite, SWT.NONE).setText("Source folder:");
-			sourceFolder = new Text(composite, SWT.NONE);
-			new Label(composite, SWT.NONE).setText("Package:");
-			pkg = new Text(composite, SWT.NONE);
-			new Label(composite, SWT.NONE).setText("Name:");
-			name = new Text(composite, SWT.NONE);
-		}
+	private InputStream openContentStream() {
+		String contents =
+			"This is the initial file contents for *.mpe file that should be word-sorted in the Preview page of the multi-page editor";
+		return new ByteArrayInputStream(contents.getBytes());
+	}
+
+	private void throwCoreException(String message) throws CoreException {
+		IStatus status =
+			new Status(IStatus.ERROR, "wyclipse", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
+
+	/**
+	 * We will accept the selection in the workbench to see if
+	 * we can initialize from it.
+	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
 	}
 }
