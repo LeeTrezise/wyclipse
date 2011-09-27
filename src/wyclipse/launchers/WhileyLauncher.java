052f170f95d912cbeca11879e7c02b91811cc73c
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-27 21:28:58 +1300

    Added a launch configuration, although it doesn't do very much!!

---
+++ src/wyclipse/launchers/WhileyLauncher.java
@@ -0,0 +1,18 @@
+package wyclipse.launchers;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class WhileyLauncher implements ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		System.out.println("LAUNCHER CALLED");
+	}	
+
+}
