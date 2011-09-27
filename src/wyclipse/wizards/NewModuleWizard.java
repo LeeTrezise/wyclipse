ef1fd957cc383e503a2bd6598202630d44aca296
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-27 19:07:52 +1300

    Working on a page for creating a new Whiley project.

--- src/wyclipse/wizards/NewModuleWizard.java
+++ src/wyclipse/wizards/NewModuleWizard.java
@@ -15,17 +15,6 @@ import java.io.*;
 import org.eclipse.ui.*;
 import org.eclipse.ui.ide.IDE;
 
-/**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "mpe". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
- */
-
 public class NewModuleWizard extends Wizard implements INewWizard {
 	private NewModulePage page;
 	private ISelection selection;
---
+++ src/wyclipse/wizards/NewProjectPage.java
@@ -0,0 +1,115 @@
+package wyclipse.wizards;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+/**
+ * The "New" wizard page allows setting the container for the new file as well
+ * as the file name. The page will only accept file name without the extension
+ * OR with the extension that matches the expected one (mpe).
+ */
+
+public class NewProjectPage extends WizardPage {
+	private Text projectName;
+	private Button defaultLocation;
+	private Text location;
+
+	private ISelection selection;
+
+	/**
+	 * Constructor for SampleNewWizardPage.
+	 * 
+	 * @param pageName
+	 */
+	public NewProjectPage(ISelection selection) {
+		super("wizardPage");
+		setTitle("Create a Whiley Project");
+		setDescription("Enter a project name.");
+		this.selection = selection;
+	}
+
+	/**
+	 * @see IDialogPage#createControl(Composite)
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();		
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;	
+		layout.marginWidth = 20;
+		Label label = new Label(container, SWT.NULL);
+		label.setText("&Project Name:");
+
+		projectName = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = 2;
+		projectName.setLayoutData(gd);
+		
+		defaultLocation = new Button(container, SWT.CHECK);				
+		defaultLocation.setText("Use default location");
+
+		location = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		location.setLayoutData(gd);
+		initialiseLocation();
+		
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowseLocation();
+			}
+		});
+		
+		new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+		
+		setControl(container);
+	}
+
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+
+	private void handleBrowseLocation() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Choose a directory for project contents");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				location.setText(((Path) result[0]).toString());
+			}
+		}
+	}
+	
+	private void initialiseLocation() {
+		// Need to figure out how determine the default location
+	}
+	
+	public String getLocation() {
+		return location.getText();
+	}
+	
+	public String getProjectName() {
+		return projectName.getText();
+	}
+}
