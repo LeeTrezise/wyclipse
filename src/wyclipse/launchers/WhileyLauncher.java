460868b92f42e5e91bca450e27df1029d7da1227
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-11-20 18:32:01 +1300

    So, the launch configuration is sort-of working.  Well, it at least runs a whiley program now!

--- src/wyclipse/launchers/WhileyLauncher.java
+++ src/wyclipse/launchers/WhileyLauncher.java
@@ -1,18 +1,49 @@
 package wyclipse.launchers;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.debug.core.ILaunch;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import org.eclipse.core.runtime.*;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.JavaLaunchDelegate;
+import org.osgi.framework.Bundle;
 
-public class WhileyLauncher implements ILaunchConfigurationDelegate {
+import wyclipse.Activator;
 
+public class WhileyLauncher extends JavaLaunchDelegate {
+
+	private static final String WHILEY_RUNTIME_JAR = "wyrt.jar";
+	
+	/**
+	 * The main objective here is to add the Whiley runtime onto the classpath.
+	 */
 	@Override
-	public void launch(ILaunchConfiguration configuration, String mode,
-			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		System.out.println("LAUNCHER CALLED");
-	}	
+	public String[] getClasspath(ILaunchConfiguration configuration)
+			throws CoreException {
+		String[] classpath = super.getClasspath(configuration);
+		String[] newClasspath = Arrays.copyOf(classpath, classpath.length + 1);
 
+		Bundle groovyBundle = Platform.getBundle("wyclipse");
+		Enumeration<URL> enu = groovyBundle.findEntries("lib",
+				WHILEY_RUNTIME_JAR, false);
+		if (enu != null && enu.hasMoreElements()) {
+			try {
+				URL jar = FileLocator.resolve(enu.nextElement());
+				newClasspath[classpath.length] = jar.getFile();
+			} catch (IOException e) {
+				throw new CoreException(
+						new Status(Status.ERROR, Activator.WYCLIPSE_BUILDER_ID,
+								"Could not find $jarName on the class path.  Please add it manually"));
+			}
+		} else {
+			throw new CoreException(
+					new Status(Status.ERROR, Activator.WYCLIPSE_BUILDER_ID,
+							"Could not find $jarName on the class path.  Please add it manually"));
+		}				
+
+		return newClasspath;
+	}
 }
+	   
