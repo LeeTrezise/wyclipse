1465dc8eb3d64fc8aaeb1724ca33334afe016890
Lee Trezise <Lee.Trezise@gmail.com>
2011-12-08 10:13:20 +1300

    Added Copyright Information and Signed off as a contributor

--- src/wyclipse/editor/WhileyPartitioner.java
+++ src/wyclipse/editor/WhileyPartitioner.java
@@ -1,4 +1,5 @@
 // Copyright (c) 2011, David J. Pearce (djp@ecs.vuw.ac.nz)
+// Copyright (c) 2011, Lee Trezise(Lee.Trezise@gmail.com)
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
